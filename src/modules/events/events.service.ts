cbd1e9d4b9079a7b50a47fd04aa50aafe0d550e5
MD Waliullah <waliullah.godev@gmail.com>
2026-03-11 22:47:09 +0600

    feat: event service

--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
-    
+    constructor(
+        private prisma: PrismaService
+    ){}
+
+    async createEvent(createEventDto: CreateEventDto): Promise<EventResponseDto> {
+        const { name, description, date, location } = createEventDto;
+
+        try {
+            const event = await this.prisma.event.create({
+            data: {
+                description,
+                date,
+                location
+            }
+        })
+        } catch (error) {
+            console.error('Error creating event:', error);
+            throw new InternalServerErrorException('Failed to create event');
+        }
+
+        return event;
+    }   
 }
