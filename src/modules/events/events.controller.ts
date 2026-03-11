90543e5046c43626c699c7a7950089c9059f680d
MD Waliullah <waliullah.godev@gmail.com>
2026-03-11 23:36:28 +0600

    fix: check organizer for creating event

--- src/modules/events/events.controller.ts
+++ src/modules/events/events.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateEventDto } from './dto/crete-event.dto';
+import { EventResponseDto } from './dto/event-response.dto';
+import { Roles } from 'src/common/decorators/role.decorators';
+import { UserRole } from '@prisma/client';
 
 @Controller('events')
 export class EventsController {
-    constructor(private readonly eventsService: EventsService){}
+    constructor(private readonly eventsService: EventsService) { }
 
     @Post('create')
+    @Roles(UserRole.ADMIN)
     @HttpCode(201)
-    @ApiOperation({ summary: 'Create a new event', description: 'Create a new event with name, description, date and location' })
+    @ApiOperation({ summary: 'Create a new event (admin only)', description: 'Create a new event with name, description, date and location' })
     @ApiResponse({
         status: 201,
         description: 'Event created successfully',
@@ -21,10 +25,10 @@ export class EventsController {
         description: 'Bad request',
     })
 
-    @ApiResponse({
-        status: 500,
-        description: 'Internal server error',
-    })
+    // @ApiResponse({
+    //     status: 500,
+    //     description: 'Internal server error',
+    // })
 
     async createEvent(@Body() createEventDto: CreateEventDto): Promise<EventResponseDto> {
         return this.eventsService.createEvent(createEventDto);
--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -1,29 +1,70 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEventDto } from './dto/crete-event.dto';
+import { EventResponseDto } from './dto/event-response.dto';
 
 @Injectable()
 export class EventsService {
     constructor(
         private prisma: PrismaService
-    ){}
+    ) { }
 
     async createEvent(createEventDto: CreateEventDto): Promise<EventResponseDto> {
-        const { name, description, date, location } = createEventDto;
+        const { title, description, startDate, endDate, location, organizerId } = createEventDto;
+
+        const existOrganizer = await this.prisma.organizer.findUnique({
+            where: { id: organizerId }
+        })
+
+        if (!existOrganizer) {
+            throw new InternalServerErrorException('Organizer not found');
+        }
 
         try {
             const event = await this.prisma.event.create({
-            data: {
-                description,
-                date,
-                location
-            }
-        })
+                data: {
+                    title,
+                    description,
+                    startDate,
+                    endDate,
+                    location,
+                    organizerId,
+                    venueId: createEventDto.venueId || null
+                },
+                select: {
+                    id: true,
+                    title: true,
+                    description: true,
+                    startDate: true,
+                    endDate: true,
+                    location: true,
+                    createdAt: true,
+                    organizerId: true,
+                    organizer: {
+                        select: {
+                            id: true,
+                            name: true,
+                            companyName: true,
+                            contactInfo: true,
+                            verified: true
+                        }
+                    },
+                    venue: {
+                        select: {
+                            id: true,
+                            name: true,
+                            address: true,
+                        }
+                    }
+                }
+            })
+
+            return event;
+            
         } catch (error) {
             console.error('Error creating event:', error);
             throw new InternalServerErrorException('Failed to create event');
         }
 
-        return event;
-    }   
+    }
 }
