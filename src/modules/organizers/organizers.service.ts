b1096c62566f4d31ee6d93f668298b2287ecca9b
MD Waliullah <waliullah.godev@gmail.com>
2026-03-12 00:36:23 +0600

    feat: OrganizersService

--- src/modules/organizers/organizers.service.ts
+++ src/modules/organizers/organizers.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateOrganizerDto } from './dto/create-organizer.dto';
+import { OrganizerResponseDto } from './dto/organizer-response.dto';
 
 @Injectable()
-export class OrganizersService {}
+export class OrganizersService {
+    constructor(private prisma: PrismaService) { }
+
+    async createOrganizer(createOrganizerDto: CreateOrganizerDto): Promise<OrganizerResponseDto> {
+        const { name, companyName, contactInfo, userId } = createOrganizerDto;
+
+        const organizer = await this.prisma.organizer.create({
+            data: {
+                name,
+                companyName,
+                contactInfo,
+                userId
+            },
+            select: {
+                id: true,
+                name: true,
+                companyName: true,
+                contactInfo: true,
+                verified: true,
+                createdAt: true,
+                userId: true
+            }
+        })
+
+        return organizer;
+    }
+}
