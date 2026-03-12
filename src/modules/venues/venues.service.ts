1017b762f7e0251f078f6854a53697b767d5f23b
MD Waliullah <waliullah.godev@gmail.com>
2026-03-12 22:28:52 +0600

    feat: create a venue in service

--- src/modules/venues/venues.service.ts
+++ src/modules/venues/venues.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class VenuesService {}
+export class VenuesService {
+    constructor(private prisma: PrismaService) { }
+
+    // create a venue
+    async createVenue(createVenueDto: any) {
+        const { name, address, mapLink } = createVenueDto;
+
+        try {
+            const venue = await this.prisma.venue.create({
+                data: {
+                    name,
+                    address,
+                    mapLink
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    address: true,
+                    mapLink: true,
+                    createdAt: true,
+                    updatedAt: true
+                }
+            });
+            return venue;
+        } catch (error) {
+            console.error('Error creating venue:', error);
+            throw new InternalServerErrorException('Failed to create venue');
+        }
+
+    }
+
+}
