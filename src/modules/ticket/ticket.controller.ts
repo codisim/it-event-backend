b7fbdab1004c57602d23cbbea46264fbe1e24213
MD Waliullah <waliullah.godev@gmail.com>
2026-03-18 01:29:50 +0600

    feat:  create new ticket

--- src/modules/ticket/ticket.controller.ts
+++ src/modules/ticket/ticket.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { TicketService } from './ticket.service';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { TicketResponseDto } from './dto/ticket-response.dto';
 
+
+@ApiTags('Ticket')
 @Controller('ticket')
-export class TicketController {}
+export class TicketController {
+    constructor(private readonly ticketService: TicketService) {}
+
+    // create new ticker
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({
+            summary: 'Create new ticket'
+    })
+
+    @ApiBody({type: CreateTicketDto})
+
+    @ApiResponse({
+        status: 201,
+        description: 'The ticket has been successfully created.',
+        type: CreateTicketDto
+    })
+
+    @ApiResponse({
+        status: 500,
+        description: 'Internal server error.'
+    })
+
+    async createTicket(@Body() createTicketDto: CreateTicketDto): Promise<TicketResponseDto> {
+        return this.ticketService.createTicket(createTicketDto);
+    }
+
+
+}
--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { TicketResponseDto } from './dto/ticket-response.dto';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+    constructor(
+        private readonly prisma: PrismaService
+    ) { }
+
+    // create new ticket
+    async createTicket(createTicketDto: CreateTicketDto): Promise<TicketResponseDto> {
+        const { userId, eventId, ticketTypeId, orderId, qrCode, status } = createTicketDto;
+
+
+        const ticketType = await this.prisma.ticketType.findUnique({
+            where: { id: ticketTypeId }
+        });
+
+        if (!ticketType)
+            throw new NotFoundException("Ticket type not found");
+
+
+        if (ticketType.soldQuantity >= ticketType.quantity)
+            throw new BadRequestException("Tickets are sold out");
+
+
+        const result = await this.prisma.$transaction(async (tx) => {
+
+            const ticket = await tx.ticket.create({
+                data: {
+                    userId,
+                    eventId,
+                    ticketTypeId,
+                    orderId,
+                    qrCode,
+                    status: status ?? "ACTIVE"
+                },
+                include: {
+                    ticketType: true
+                }
+            });
+
+            await tx.ticketType.update({
+                where: { id: ticketTypeId },
+
+                data: {
+                    soldQuantity: {
+                        increment: 1
+                    }
+                }
+            })
+
+            return ticket;
+
+        });
+
+        return result;
+
+    }
+
+
+}
